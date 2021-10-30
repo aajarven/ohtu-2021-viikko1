6bded8a986790f2d71258cb21dc5ff673b06db22
Anni Järvenpää <anni.jarvenpaa@gmail.com>
2021-10-30 11:24:08 +0000

    riko testit

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -40,6 +40,9 @@ class TestVarasto(unittest.TestCase):
         self.assertEqual(str(self.varasto),
                          "saldo = 0, vielä tilaa 10")
 
+    def test_jotain_rikkinaista(self):
+        self.assertEqual(self.varasto.saldo, 999)
+
 
 class TestEsitaytettyVarasto(unittest.TestCase):
 
